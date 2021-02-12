6edd58c995525adc275dae36c9f714ed9d671229
Hari Krishnan <harikrishnan83@gmail.com>
2021-02-12 17:40:45 +0530

    adding docker network

--- run-performance-tests.sh
+++ run-performance-tests.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 mkdir -p .m2
-docker run -it --rm --name performance-testing \
+docker run -it --rm --name perfiz-gatling \
   -v "$(pwd)/.m2":/root/.m2 \
   -v "$(pwd)":/usr/src/performance-testing \
   -v "$1":/usr/src/karate-features \
   -v "$2":/usr/src/perfiz.yaml \
   -e KARATE_FEATURES=/usr/src/karate-features \
   -w /usr/src/performance-testing \
+  --network perfiz-network \
   maven:3.6-jdk-8 mvn clean test-compile gatling:test -DPERFIZ=/usr/src/perfiz.yaml
