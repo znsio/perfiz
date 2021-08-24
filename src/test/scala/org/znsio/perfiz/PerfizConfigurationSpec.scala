1468a79a50821d11ce86c6534402d24e0c5d13d6
Hari Krishnan <harikrishnan83@gmail.com>
2021-08-24 14:57:37 +0530

    separating unit tests and gatling tests

--- src/test/scala/org/znsio/perfiz/PerfizConfigurationSpec.scala
+++ src/test/scala/org/znsio/perfiz/PerfizConfigurationSpec.scala
@@ -3,7 +3,7 @@ package org.znsio.perfiz
 import org.junit.Assert.assertNotNull
 import org.junit._
 
-class PerfizConfigurationSuite {
+class PerfizConfigurationSpec {
   @Test def `load perfiz configuration yaml`: Unit = {
     System.setProperty("PERFIZ", this.getClass.getResource("/perfiz.yaml").getPath)
     assertNotNull(PerfizConfiguration())
